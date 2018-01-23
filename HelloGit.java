0dd252fcce445c50b42531c94b8e765ab05ac223
ishu250396 <33687428+ishu250396@users.noreply.github.com>
2018-01-23 16:05:23 +0530

    New Method has added

--- HelloGit.java
+++ HelloGit.java
@@ -3,6 +3,12 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Git");
+	message();
 	}
+ public static void message()
+ {
+	 System.out.println("Welcome to gitHub");
+ }
 
 }
+
